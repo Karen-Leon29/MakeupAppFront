4b2078168701bee66e3a53f1c175b4d463e43eca
Aimar Bayona <a.bayona@nelumbo.com.co>
2024-10-06 23:02:56 -0500

    new request flow

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { AppProvider } from 'core/contexts/index.tsx'
+import { AppLanguages } from 'core/enum/appLanguages.enum.tsx'
+import { LitteraProvider } from '@assembless/react-littera'
+import { ThemeProvider } from '@emotion/react'
+import theme from 'core/theme/theme.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <AppProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </AppProvider>
+  <LitteraProvider locales={[AppLanguages.EN, AppLanguages.ES]} initialLocale={AppLanguages.ES}>
+    <ThemeProvider theme={theme}>
+      <AppProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </AppProvider>
+    </ThemeProvider>
+  </LitteraProvider>
 )
--- src/modules/homeoage/components/navbar/index.tsx
+++ src/modules/homeoage/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   AppBar,
   Toolbar,
@@ -9,6 +9,12 @@ import {
   IconButton,
   InputBase,
   Badge,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
 } from '@mui/material'
 import {
   Search as SearchIcon,
@@ -21,7 +27,8 @@ import { useStyles } from './styles'
 import { useNavigate } from 'react-router-dom'
 
 export const NavbarComponent: React.FC = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [openDialog, setOpenDialog] = useState(false) 
   const classes = useStyles
   const navigate = useNavigate()
 
@@ -33,65 +40,95 @@ export const NavbarComponent: React.FC = () => {
     setAnchorEl(null)
   }
 
+  const handleOpenDialog = () => {
+    setOpenDialog(true)
+  }
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false)
+  }
+
+  const handleLogout = () => {
+    handleCloseDialog()
+    navigate('/') 
+  }
+
   return (
-    <AppBar position="static" sx={classes.appBar}>
-      <Toolbar sx={classes.toolbar}>
-        <Typography sx={classes.logo}>Logo</Typography>
-        <Box sx={classes.linksContainer}>
-          <Box onClick={() => navigate('/homepage')}>
-            <Typography sx={classes.link}>Inicio</Typography>
-          </Box>
-          <Typography
-            sx={{ ...classes.link, display: 'flex', alignItems: 'center' }}
-            onClick={handleOpenMenu}
-          >
-            Categorías <ExpandMoreIcon />
-          </Typography>
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleCloseMenu}
-            MenuListProps={{
-              'aria-labelledby': 'categories-button',
-            }}
-          >
-            <MenuItem onClick={handleCloseMenu}>Belleza</MenuItem>
-            <MenuItem onClick={handleCloseMenu}>Perfumería</MenuItem>
-            <MenuItem onClick={handleCloseMenu}>Maquillaje</MenuItem>
-            <MenuItem onClick={handleCloseMenu}>Cuidado de la Piel</MenuItem>
-            <MenuItem onClick={handleCloseMenu}>Cuidado del Cabello</MenuItem>
-            <MenuItem onClick={handleCloseMenu}>Manicura</MenuItem>
-            <MenuItem onClick={handleCloseMenu}>Spa y Relajación</MenuItem>
-          </Menu>
-          <Box onClick={() => navigate('/about-us')}>
-            <Typography sx={classes.link}>Sobre Nosotros</Typography>
-          </Box>
-          <Typography sx={classes.link}>Contáctanos</Typography>
-        </Box>
-        <Box display="flex" alignItems="center" gap={2}>
-          <Box sx={classes.search}>
-            <InputBase
-              sx={classes.inputBase}
-              placeholder="Buscar..."
-              inputProps={{ 'aria-label': 'search' }}
-            />
-            <Box sx={classes.searchIconWrapper}>
-              <SearchIcon />
+    <>
+      <AppBar position="static" sx={classes.appBar}>
+        <Toolbar sx={classes.toolbar}>
+          <Typography sx={classes.logo}>Logo</Typography>
+          <Box sx={classes.linksContainer}>
+            <Box onClick={() => navigate('/homepage')}>
+              <Typography sx={classes.link}>Inicio</Typography>
             </Box>
+            <Typography
+              sx={{ ...classes.link, display: 'flex', alignItems: 'center' }}
+              onClick={handleOpenMenu}
+            >
+              Categorías <ExpandMoreIcon />
+            </Typography>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleCloseMenu}
+              MenuListProps={{
+                'aria-labelledby': 'categories-button',
+              }}
+            >
+              <MenuItem onClick={handleCloseMenu}>Belleza</MenuItem>
+              <MenuItem onClick={handleCloseMenu}>Perfumería</MenuItem>
+              <MenuItem onClick={handleCloseMenu}>Maquillaje</MenuItem>
+              <MenuItem onClick={handleCloseMenu}>Cuidado de la Piel</MenuItem>
+              <MenuItem onClick={handleCloseMenu}>Cuidado del Cabello</MenuItem>
+              <MenuItem onClick={handleCloseMenu}>Manicura</MenuItem>
+              <MenuItem onClick={handleCloseMenu}>Spa y Relajación</MenuItem>
+            </Menu>
+            <Box onClick={() => navigate('/about-us')}>
+              <Typography sx={classes.link}>Sobre Nosotros</Typography>
+            </Box>
+            <Typography sx={classes.link}>Contáctanos</Typography>
           </Box>
-          <IconButton sx={classes.iconButton}>
-            <PersonIcon />
-          </IconButton>
-          <IconButton sx={classes.iconButton}>
-            <Badge badgeContent={4} color="error">
-              <ShoppingCartIcon />
-            </Badge>
-          </IconButton>
-          <IconButton sx={classes.iconButton}>
-            <LocalShippingIcon />
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
+          <Box display="flex" alignItems="center" gap={2}>
+            <Box sx={classes.search}>
+              <InputBase
+                sx={classes.inputBase}
+                placeholder="Buscar..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+              <Box sx={classes.searchIconWrapper}>
+                <SearchIcon />
+              </Box>
+            </Box>
+            <IconButton sx={classes.iconButton} onClick={handleOpenDialog}>
+              <PersonIcon />
+            </IconButton>
+            <IconButton sx={classes.iconButton}>
+              <Badge badgeContent={4} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+            <IconButton sx={classes.iconButton}>
+              <LocalShippingIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Cerrar Sesión</DialogTitle>
+        <DialogContent>
+          <DialogContentText>¿Estás seguro de que deseas cerrar sesión?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleLogout} color="primary" autoFocus>
+            Aceptar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
